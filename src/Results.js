1964a9ecea95bd4357148901e96c066d38a1e9b9
tam3marie <tam3marie@icloud.com>
2021-09-14 15:59:51 -0400

    Added Rendering of the Phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meanings from "./Meanings";
 
 export default function Results(props) {
@@ -6,6 +7,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.response.word}</h2>
+        {props.response.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.response.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
