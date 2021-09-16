9b337ec1680dbe528b8eb816f9ebc59c5d8d1cc3
tam3marie <tam3marie@icloud.com>
2021-09-16 10:49:17 -0400

    Updated Default Keyword on Load

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,49 +4,63 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("newspaper");
   const [response, setResponse] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResponse(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  function search() {
     // API Documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
-  return (
-    <div className="Dictionary">
-      <hr className="thick-break" />
-      <hr className="thin-break" />
-      <form onSubmit={search}>
-        <div className="row">
-          <span className="col-4">
-            <span className="form-header-left">Look up any word!</span>
-          </span>
-          <span className="col-4">
-            <input
-              type="search"
-              placeholder="Type a word"
-              onChange={handleKeywordChange}
-            />
-            <input type="submit" value="Search" />
-          </span>
-          <span className="col-4">
-            <span className="form-header-right">Discover anything!</span>
-          </span>
-        </div>
-      </form>
-      <hr className="thin-break" />
-      <hr className="thick-break" />
-      <Results response={response} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <hr className="thick-break" />
+        <hr className="thin-break" />
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <span className="col-4">
+              <span className="form-header-left">Look up any word!</span>
+            </span>
+            <span className="col-4">
+              <input
+                type="search"
+                placeholder="Type a word"
+                onChange={handleKeywordChange}
+              />
+              <input type="submit" value="Search" />
+            </span>
+            <span className="col-4">
+              <span className="form-header-right">Discover anything!</span>
+            </span>
+          </div>
+        </form>
+        <hr className="thin-break" />
+        <hr className="thick-break" />
+        <Results response={response} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading...";
+  }
 }
